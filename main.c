6be9f0e65e9ec7854e6f9b678f2d16b63d9d632a
olkiz <10522249+olkiz@users.noreply.github.com>
2023-01-08 22:22:09 +0200

    Added output to USB-com

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 int main()
 {
+    stdio_init_all();
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     while (true)
     {
         gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        printf("LED ON\n");
         sleep_ms(100);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        printf("LED OFF\n");
         sleep_ms(100);
     }
 }
