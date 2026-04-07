d7908e2d791f070664ed797daa9a17313a7539f9
olkiz <olkiz@proton.me>
2026-04-07 23:04:04 +0300

    Moved accel and gyro get raw values functions to private section for mpu9250

--- src/MPU9250.h
+++ src/MPU9250.h
@@ -9,16 +9,17 @@ public:
 
     void reset();
 
-    void readAccelerometerRaw(int16_t acceleration[3]);
-    void readGyroRaw(int16_t gyro[3]);
-
     void readMagRaw(int16_t mag[3]);
+
     void readGyro(double gyro[3]);
     void readAccelerometer(double acceleration[3]);
 
     int temperature();
 
 private:
+    void readAccelerometerRaw(int16_t acceleration[3]);
+    void readGyroRaw(int16_t gyro[3]);
+
     void magInit(uint8_t scale, uint8_t mode);
 
     void read(uint8_t reg, uint8_t *buf, uint16_t len);
